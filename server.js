d8484012cce8b398b20b1e396d7e973c195e6a46
Bralcorn <bralcrn@asu.edu>
2018-06-26 03:31:03 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Create a Github repo called simple-http-node-server. Create an 
+// HTTP server on port 3000 with a request handler that creates a 
+// file named hello-world.txt. You will be using the fs module to 
+// do this. Write the content: "Hello to this great world" to the 
+// hello-world.txt file. Please submit your finished code in your 
+// submission.
+
+
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end(`Handling a request on port ${port}`)
+  try {
+    fs.appendFileSync('hello-world.txt', 'Hello to this great world');
+    console.log('The "data to append" was appended to file!');
+  } catch (err) {
+  }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+
